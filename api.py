e9c33fef79b3d23cc304d42dca068ce5e46c5c56
Arindam <arindamghoshindia96@gmail.com>
2023-05-13 20:51:14 +0200

    pre-prompt added

--- api.py
+++ api.py
@@ -28,7 +28,7 @@ def generate_story(player_id: int, player_input: str):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a storyteller in Barcelona."},
+            {"role": "system", "content": "I want you to act as an engaging storyteller, the story would be set in Barcelona, including all the popular places Barcelona has. You will come up with entertaining stories that are engaging, imaginative, and captivating for the user. It can be suspense or thriller or any other type of story which has the potential to capture peoples attention and imagination. Your response should be in the format of \nAI - Here it would give context.\nAI - Will ask a question and give probable options to choose from by the user. Rules for how the options should be \n- options should look like this {{Num}} Option, where Num is the serial number \n- options should not be more than 3 words. \n-There only can be a maximum of 5 choices but try to keep 3 choices mostly.\n Next, Wait for the user to choose an option. My first request is \"I need an engaging choice-based story set in Barcelona\""},
             {"role": "user", "content": player_input},
             {"role": "assistant", "content": story},
         ],
